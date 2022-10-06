251c4719485be335485d352536f8f682cbf74634
Artem Semenov <tsedro00@yandex.ru>
2022-10-06 17:43:51 +0300

    Done: homework_1

---
+++ homework_1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "homework_1/solver"
+
+const (
+	precision float64 = 0.0001
+	step      float64 = 0.5
+	max       float64 = 5
+)
+
+func main() {
+	solver.Table(0, max, step, precision)
+}
---
+++ homework_1/solver/arg.go
@@ -0,0 +1,12 @@
+package solver
+
+import (
+	"fmt"
+
+	helper "github.com/daniel228228/golang-learning-helper"
+)
+
+func Argument(max, value, precision float64) {
+	x, iter := helper.GetXByValue(0, max, value, precision)
+	fmt.Printf("check:for f(x)=%f x =%f(%d iteration)\n", value, x, iter)
+}

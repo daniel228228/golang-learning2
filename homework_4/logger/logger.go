3f0b466ce5b184d295d411b14614e05f09c29e41
daniel_228 <danielshevtsov@ya.ru>
2022-11-05 00:10:59 +0300

    Done: homework_4

--- homework_4/logger/logger.go
+++ homework_4/logger/logger.go
@@ -1,9 +1,53 @@
 package logger
 
+import (
+	"fmt"
+	"io"
+)
+
 type logger struct {
-	// TODO: implement me
+	debugLabel string
+	output     io.Writer
 }
 
 func NewLogger() *logger {
-	// TODO: implement me
+	return &logger{}
+}
+
+func (l *logger) Debug(args ...any) {
+	l.Debugf("%s", fmt.Sprint(args...))
+}
+
+func (l *logger) Debugf(format string, args ...any) {
+	if l.debugLabel != "" {
+		fmt.Fprintf(l.output, "%s"+format+"\n", append([]any{l.debugLabel}, args...)...)
+	}
+}
+
+func (l *logger) Print(args ...any) {
+	l.Printf("%s", fmt.Sprint(args...))
+}
+
+func (l *logger) Printf(format string, args ...any) {
+	fmt.Fprintf(l.output, "%s"+format+"\n", append([]any{l.debugLabel}, args...)...)
+}
+
+func (l *logger) Error(args ...any) {
+	l.Errorf("%s", fmt.Sprint(args...))
+}
+
+func (l *logger) Errorf(format string, args ...any) {
+	fmt.Fprintf(l.output, "%s%s"+format+"\n", append([]any{l.debugLabel, "[ERROR] "}, args...)...)
+}
+
+func (l *logger) SetOutput(writer io.Writer) {
+	l.output = writer
+}
+
+func (l *logger) SetDebugMode(debugMode bool) {
+	if debugMode {
+		l.debugLabel = "[DEBUG] "
+	} else {
+		l.debugLabel = ""
+	}
 }
